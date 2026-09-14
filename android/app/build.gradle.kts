685da96aa5bc8f95e09d6314ae8fd635891fa1ce
magic76 <shanliyoung@gmail.com>
2026-09-14 15:48:43 +0800

    build: bundle native adapter with Forge web runtime

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,6 +40,7 @@ val syncForgeWeb by tasks.registering(Sync::class) {
     from(repoRoot.file("index.html"))
     from(repoRoot.file("forge.css"))
     from(repoRoot.file("forge.js"))
+    from(repoRoot.file("native-adapter.js"))
 }
 
 tasks.named("preBuild").configure { dependsOn(syncForgeWeb) }
